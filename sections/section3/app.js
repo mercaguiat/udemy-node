a78af45f087a9a378194e111f312ada64f01f135
Merci <160075925+mercaguiat@users.noreply.github.com>
2025-03-23 14:37:24 +0800

    35. Parsing Request Bodies

--- sections/section3/app.js
+++ sections/section3/app.js
@@ -13,6 +13,16 @@ const server = http.createServer((req, res) =>{ // Creating an HTTP server
         return res.end();
     }
     if (url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
         fs.writeFileSync('message.txt', 'DUMMY');
         res.statusCode = 302;
         res.setHeader('Location', '/');
