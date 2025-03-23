77ebd52ed195431e3ca516b30331af5a651372c8
Merci <160075925+mercaguiat@users.noreply.github.com>
2025-03-23 13:55:20 +0800

    30. Understanding Requests

--- sections/section3/app.js
+++ sections/section3/app.js
@@ -1,7 +1,8 @@
 const http = require('http'); // Importing the built-in 'http' module
 
 const server = http.createServer((req, res) =>{ // Creating an HTTP server
-    console.log(req) // Log the incoming request object to the console
+    console.log(req.url, req.method, req.headers); // Log the incoming request object to the console
+    // process.exit() //Hard exits event loop
 });
 
 server.listen(3000); // Listening on port 3000 for incoming connections
