3841aebb241c1dd21726d12e9c74929a9681eb8c
Merci <160075925+mercaguiat@users.noreply.github.com>
2025-03-23 15:09:47 +0800

    37. Blocking and Non-Blocking Code

--- sections/section3/app.js
+++ sections/section3/app.js
@@ -18,14 +18,14 @@ const server = http.createServer((req, res) =>{ // Creating an HTTP server
             console.log(chunk);
             body.push(chunk);
         }); 
-        // This is wrong according to Max
-        req.on('end', () =>{
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
     }
     res.setHeader('Content-Type', 'text/html');
