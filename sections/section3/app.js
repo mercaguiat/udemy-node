80c17a3f2f79a1bced33946c153e655646253658
Merci <160075925+mercaguiat@users.noreply.github.com>
2025-03-23 14:18:57 +0800

    31. Sending Responses

--- sections/section3/app.js
+++ sections/section3/app.js
@@ -2,7 +2,12 @@ const http = require('http'); // Importing the built-in 'http' module
 
 const server = http.createServer((req, res) =>{ // Creating an HTTP server
     console.log(req.url, req.method, req.headers); // Log the incoming request object to the console
-    // process.exit() //Hard exits event loop
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>hello</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000); // Listening on port 3000 for incoming connections
