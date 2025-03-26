22e24cbe33b9550182e949218d054dce6e103931
Merci <160075925+mercaguiat@users.noreply.github.com>
2025-03-26 19:24:40 +0800

    90. Converting our Project to Handlebars

--- sections/section6/routes/shop.js
+++ sections/section6/routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/',(req, res, next)=>{
     const products = adminData.products;
-    res.render('shop',{prods: products, pageTitle: 'Shop', path:'/shop'});
+    res.render('shop',{prods: products, pageTitle: 'Shop', path:'/shop', hasProducts: products.length > 0});
 })
 
 module.exports = router;
