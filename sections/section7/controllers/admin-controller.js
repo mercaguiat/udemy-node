5c3e7bedc87509aad9804d5e0e6f4585bc9e9848
Merci <160075925+mercaguiat@users.noreply.github.com>
2025-04-02 18:36:35 +0800

    125. Using Query Params

--- sections/section7/controllers/admin-controller.js
+++ sections/section7/controllers/admin-controller.js
@@ -1,11 +1,9 @@
 const Product = require("../models/product-model");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product",
-    productCSS: true,
-    activeAddProduct: true,
+    path: "/admin/add-product"
   });
 };
 
@@ -17,6 +15,18 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  res.render("admin/edit-product", {
+    pageTitle: "Edit Product",
+    path: "/admin/edit-product",
+    editing: editMode
+  });
 };
 
 exports.getProducts = (req, res, next) => {
