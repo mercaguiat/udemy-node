75b29d3f528373b6fb1ea50d79d69c857a3b5933
Merci <160075925+mercaguiat@users.noreply.github.com>
2025-04-06 02:06:06 +0800

    130. Deleting Cart Items

--- sections/section7/models/cart-model.js
+++ sections/section7/models/cart-model.js
@@ -35,4 +35,23 @@ module.exports = class Cart {
             })
         });
     }
+
+     static deleteProduct(id, price){
+        fs.readFile(p, (err, fileContent) =>{
+            let cart = {products: [], totalPrice: 0};
+            if (!err) {
+                return;
+            }
+            const updatedCart = {...JSON.parse(fileContent)};
+            const product = updatedCart.products.find(prod => prod.id === id);
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice = 
+                updatedCart.totalPrice - productPrice * productQty;
+
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            })
+        });
+     }
 }
